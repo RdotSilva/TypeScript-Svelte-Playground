9ace3516724e99a60d280ce8c03007e2ce7ecf9a
RdotSilva <RyandotSilva@gmail.com>
2023-05-17 21:23:11 +0100

    Add rollupOptions example

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // build: {
+  //   rollupOptions: {
+  //     input: "src/changedEntryPoint.ts", // Example of changing entry point
+  //   },
+  // },
   plugins: [svelte()],
-})
+});

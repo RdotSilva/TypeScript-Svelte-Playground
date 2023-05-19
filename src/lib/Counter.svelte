f3bbf2b1da1a6038d1926f457d85b0a2d2dcdca6
RdotSilva <RyandotSilva@gmail.com>
2023-05-19 14:59:31 +0100

    Add reactive statement examples

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,22 @@
 <script>
   let count = 0;
 
+  // let countClick;
+
+  // Exclude the backets to initialize variable
+  $: countClick = `You have clicked ${count} times.`;
+
+  $: if (count > 5) {
+    console.log(`Reactive state count is ${count}`);
+  }
+
   const increment = () => {
     count += 1;
   };
 </script>
+
+<h3>{countClick}</h3>
+<h3>You can clicked {count} times</h3>
 
 <button on:click={increment}>Clicks {count}</button>
 

d89c5fb51b8d8af7692bf0cc22251159f66d7e56
RdotSilva <RyandotSilva@gmail.com>
2023-05-21 15:53:59 -0400

    Add ReactiveCounter component

---
+++ src/lib/ReactiveCounter.svelte
@@ -0,0 +1,31 @@
+<script>
+  let count = 0;
+  let countTwo = 0;
+
+  const setCountTwo = (newCount) => {
+    countTwo = newCount;
+  };
+
+  // Order of the statements is important
+  $: setCountTwo(count);
+  $: countTwoTotal = `countTwo is ${countTwo}`;
+
+  const getTotal = () => {
+    return count + countTwo;
+  };
+
+  const increment = () => {
+    count += 1;
+  };
+
+  const incrementTwo = () => {
+    count += 1;
+  };
+</script>
+
+<h3>You can clicked {count} times</h3>
+
+<button on:click={increment}>Clicks {count}</button>
+<button on:click={incrementTwo}>Clicks {countTwo}</button>
+
+

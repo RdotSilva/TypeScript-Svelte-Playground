875721013ee6fc9b711b54876f4fc9321074385a
RdotSilva <RyandotSilva@gmail.com>
2023-05-25 12:24:28 -0400

    Add config for scss

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,13 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+import preprocess from "svelte-preprocess";
+
+export const config = {
+  preprocess: preprocess({
+    scss: {},
+  }),
+};
+
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors

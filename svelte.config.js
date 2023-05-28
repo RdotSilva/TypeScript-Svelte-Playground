7a6d4229f7e204f3e2d6349fe2d5b225786a7d78
RdotSilva <RyandotSilva@gmail.com>
2023-05-28 21:08:13 +0100

    Update config for multiple preprocesses

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
 import preprocess from "svelte-preprocess";
 
-export const config = {
-  preprocess: preprocess({
-    scss: { preprendData: "" },
-  }),
-};
-
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+  preprocess: preprocess({
+    scss: { prependData: "" },
+  }),
   preprocess: vitePreprocess(),
 };

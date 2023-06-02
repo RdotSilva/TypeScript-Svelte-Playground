066f4d8fde289bd7338632cf9c80bbb701c44889
RdotSilva <RyandotSilva@gmail.com>
2023-06-02 21:25:03 +0100

    Archive button app

---
+++ src/archive/Archive-App-Button.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Button from "../lib/Button.svelte";
+  import Counter from "../lib/Counter.svelte";
+  import ReactiveCounter from "../lib/ReactiveCounter.svelte";
+  import DiBower from "svelte-icons/di/DiBower.svelte";
+
+  const counterProps = {
+    maxCount: 10,
+    initialCount: 3,
+  };
+</script>
+
+<Button let:isLeftHovered size="small" shadow bgColor="red" textColor="blue">
+  <div style:width="20px" slot="leftContent">
+    {#if isLeftHovered}
+      <DiBower />
+    {:else}
+      <div>.</div>
+    {/if}
+  </div>
+  Button Text</Button
+>
+
+<Counter {...counterProps} />
+
+<ReactiveCounter shouldCount="true" />
+
+<style>
+</style>

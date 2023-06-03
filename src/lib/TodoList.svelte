6005c6e169dfe24849cb41ccf598be71765634be
RdotSilva <RyandotSilva@gmail.com>
2023-06-03 21:29:33 +0100

    Update list rendering with numbers

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <ul>
-  {#each todos as todo}
-    <li>{todo.title}</li>
+  {#each todos as { id, title }, index (id)}
+    {@const number = index + 1}
+    <li>{number}- {title}</li>
   {/each}
 </ul>

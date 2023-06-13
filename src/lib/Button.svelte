41b9e7d02ee580198aa0effc61d569a659cd710e
RdotSilva <RyandotSilva@gmail.com>
2023-06-13 22:00:19 +0100

    Update props for Button component

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -8,12 +8,14 @@
 </script>
 
 <button
+  on:click
   style:--buttonBgColor={bgColor}
   style:--buttonTextColor={textColor}
   class:size-lg={size === "large"}
   class:size-sm={size === "small"}
   class:has-left={$$slots.leftContent}
   class:shadow
+  {...$$restProps}
 >
   {#if $$slots.leftContent}
     <div
@@ -28,32 +30,34 @@
 </button>
 
 <style lang="scss">
-  @use "../../src/styles/variables.scss";
   button {
     display: flex;
     align-items: center;
     border: none;
     background-color: var(--buttonBgColor);
     color: var(--buttonTextColor);
-    padding: 15px 20px;
     font-weight: bold;
     border-radius: 5px;
     cursor: pointer;
     .left-content {
       margin-right: 10px;
     }
-    &:hover {
-      background-image: linear-gradient(rgba(0, 0, 0, 0.4), 0 0);
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
     }
-
+    &:hover {
+      background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
+    }
     &:active {
-      background-image: linear-gradient(rgba(255, 255, 255, 0.4), 0 0);
+      background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
     }
     &.size-sm {
       padding: 15px 20px;
     }
     &.size-lg {
       padding: 20px 25px;
+      font-size: 20px;
     }
     &.shadow {
       box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);

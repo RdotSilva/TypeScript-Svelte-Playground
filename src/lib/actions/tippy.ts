79564e2ca4f995cffbba60f7164fe3716f64e6ed
RdotSilva <RyandotSilva@gmail.com>
2023-06-26 19:29:17 +0100

    Add tippyAction function

---
+++ src/lib/actions/tippy.ts
@@ -0,0 +1,7 @@
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/light.css";
+
+export const tippyAction = (node, options) => {
+  const instance = tippy(node, options);
+};

ed1e044db990fbe4430758ea9b0e3aa2137f5e17
RdotSilva <RyandotSilva@gmail.com>
2023-06-28 17:58:58 +0100

    Add settings input

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,1 +1,20 @@
+<script>
+
 <h2>Settings</h2>
+
+<label>
+  <input
+    type="radio"
+    bind:group={colorScheme}
+    name="colorScheme"
+    value="dark"
+  /> Dark
+</label>
+<label>
+  <input
+    type="radio"
+    bind:group={colorScheme}
+    name="colorScheme"
+    value="light"
+  /> Light
+</label>

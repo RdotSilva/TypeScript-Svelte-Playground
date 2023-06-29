a94842d8b10fb661742651be5babe0b9cf21ca62
RdotSilva <RyandotSilva@gmail.com>
2023-06-29 17:34:29 +0100

    Refactor using direct updates to store

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,27 +1,19 @@
 <script>
-  import { onDestroy } from "svelte";
   import settings from "../stores/settings";
-
-  let colorScheme;
-
-  const unsubscribe = settings.subscribe((settings) => {
-    console.log("settings subscribe");
-    colorScheme = settings.colorScheme;
-  });
-
-  $: {
-    settings.update((prevSettings) => ({ ...prevSettings, colorScheme }));
-  }
-
-  onDestroy(unsubscribe);
 </script>
 
 <h2>Settings</h2>
 
+<button
+  on:click={() => {
+    $settings.colorScheme = $settings.colorScheme === "dark" ? "light" : "dark";
+  }}>Toggle Color Scheme</button
+>
+{$settings.colorScheme}
 <label>
   <input
     type="radio"
-    bind:group={colorScheme}
+    bind:group={$settings.colorScheme}
     name="colorScheme"
     value="dark"
   /> Dark
@@ -29,7 +21,7 @@
 <label>
   <input
     type="radio"
-    bind:group={colorScheme}
+    bind:group={$settings.colorScheme}
     name="colorScheme"
     value="light"
   /> Light

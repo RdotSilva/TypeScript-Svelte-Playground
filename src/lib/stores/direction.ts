a3c58b735f1e9c5f4dbd675f7f0487c4fec3c2fa
RdotSilva <RyandotSilva@gmail.com>
2023-07-05 19:42:30 +0100

    Add timer logic to direction store

--- src/lib/stores/direction.ts
+++ src/lib/stores/direction.ts
@@ -2,8 +2,18 @@ import { derived } from "svelte/store";
 import settings from "./settings";
 import location from "./location";
 
-const direction = derived(settings, ($settings) => {
-  return $settings.language === "ar" ? "rtl" : "ltr";
-});
+const direction = derived(
+  [settings, location],
+  ([$settings, $location], set) => {
+    console.log($settings, $location);
+    const timer = setTimeout(() => {
+      set($settings.language === "ar" ? "rtl" : "ltr");
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  },
+  "not decided"
+);
 
 export default direction;

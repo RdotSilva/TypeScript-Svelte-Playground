fe42f1ceef89af5f904dc034c7b84ab0eadd9620
RdotSilva <RyandotSilva@gmail.com>
2023-07-10 19:07:20 +0100

    Add logic for setting context

--- src/lib/Form/Form.svelte
+++ src/lib/Form/Form.svelte
@@ -1,4 +1,11 @@
-<script></script>
+<script>
+  import { setContext } from "svelte";
+  import formKey from "./formKey";
+
+  export let initialValues = {};
+
+  setContext(formKey, { values: initialValues });
+</script>
 
 <form>
   <slot />

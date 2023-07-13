3f3f9da34df3b47d16983fd4d0f56ebb760fde09
RdotSilva <RyandotSilva@gmail.com>
2023-07-13 18:52:54 +0100

    Add validateEmail util

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -2,7 +2,7 @@
  * Validate that a required value is present
  * @param value The value you want to validate
  * @param label The label of the value you want to validate
- * @returns 
+ * @returns
  */
 export const validateRequiredField = (value, label = "Field") => {
   let error;
@@ -12,3 +12,16 @@ export const validateRequiredField = (value, label = "Field") => {
   return error;
 };
 
+/**
+ * Validate if an email is present and valid
+ * @param value The value of the email
+ * @param label The label of the email
+ * @returns
+ */
+export const validateEmail = (value, label = "Email") => {
+  let error;
+  if (value && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+    error = `The ${label} is Invalid.`;
+  }
+  return error;
+};

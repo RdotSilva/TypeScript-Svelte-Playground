8d99ad40d136f93cf5eba115c19dcc91a4a4b056
RdotSilva <RyandotSilva@gmail.com>
2023-07-17 17:33:19 -0400

    Add spin transition

---
+++ src/lib/transitions/spin.ts
@@ -0,0 +1,23 @@
+import { cubicInOut } from "svelte/easing";
+
+export const spin = (
+  node,
+  { delay = 0, duration = 400, easing = cubicInOut, spin = 1 }
+) => {
+  const originalOpacity = +getComputedStyle(node).opacity;
+  const originalTransform = getComputedStyle(node).transform.replace(
+    "none",
+    ""
+  );
+  return {
+    delay,
+    duration,
+    easing,
+    css: (t) => {
+      return `
+        opacity: ${t * originalOpacity};
+        transform: ${originalTransform} scale(${t}) rotate(${spin * 360 * t}deg)
+      `;
+    },
+  };
+};
